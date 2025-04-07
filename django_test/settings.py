79575e6a5a57bb9af017d7d8410658b134a38537
Yen Ha <nhuha435@gmail.com>
2025-04-06 19:41:12 -0500

    configuring for heroku

--- django_test/settings.py
+++ django_test/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-th_u&hlas%)d!=(ejfi5^8)#9buiup5rt-o%zswv_i3$i=5fr8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
